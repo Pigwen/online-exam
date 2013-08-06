f8bf9d1a5514e9afff906b0d423b3bbd0ccfcbf8
Pigwen <sitereginfo@gmail.com>
2013-08-06 23:02:14 +0800

    start implement create choice logic

--- app/controllers/Choices.scala
+++ app/controllers/Choices.scala
@@ -3,13 +3,27 @@ package controllers
 import play.api.mvc.Action
 import play.api.mvc.Controller
 import models.Db._
+import play.api.data.Form
+import play.api.data.Forms._
+import views.html.defaultpages.badRequest
 
 object Choices extends Controller {
+  private val form = Form(mapping(
+    "id" -> longNumber,
+    "title" -> nonEmptyText(1),
+    "answer" -> nonEmptyText)(Choice.apply)(Choice.unapply))
+
   def index = Action {
     Ok(views.html.choices.index(ChoicesTb.findAll))
   }
-  
-  def createForm = Action {
+
+  def createForm = Action { implicit request =>
     Ok(views.html.choices.form())
+  }
+
+  def create = Action { implicit request =>
+    form.bindFromRequest.fold(
+      formWithError => BadRequest,
+      choice => Redirect(routes.Choices.index))
   }
 }
